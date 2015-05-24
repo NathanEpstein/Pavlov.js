cfd1bc74540f802ef06f9326f44714474e5a61dd
Nathan <ne2210@columbia.edu>
2015-05-24 16:55:42 -0400

    different types of testing data

---
+++ spec/test/randomData.js
@@ -0,0 +1,15 @@
+var obs1 = [
+  {state:"notPrize", action:"move"},
+  {state:"prize", action:"stay"},
+  {state:"prize", action:"stay"},
+  {state:"notPrize",action:"move"},
+  {state:"notPrize",action:"move"}
+]
+
+var obs2 = [
+  {state:"notPrize", action:"stay"},
+  {state:"notPrize", action:"stay"},
+  {state:"prize", action:"move"},
+  {state:"prize", action:"move"},
+  {state:"notPrize", action:"move"},
+]
--- spec/test/testSpec.js
+++ spec/test/testSpec.js
@@ -1,10 +1,34 @@
 'use strict';
 
 var pavlov = require('../../pavlov.js');
-var data = require('./testData.js')
+var determ = require('./deterministicData.js');
+var random = require('./randomData.js');
 
-describe('rewardsAndTransitions',function(){
-  it('should produce the correct rewards',function(){
-    console.log(pavlov.rewardsAndTransitions(data.observations,data.rewards));
+describe('deterministic rewardsAndTransitions',function(){
+  var rewardsAndTransitions = pavlov.rewardsAndTransitions(determ.observations,determ.rewards);
+  var R = rewardsAndTransitions[1];
+  var P = rewardsAndTransitions[0];
+
+  it('should produce the correct transitions',function(){
+    // expect P['A']['F']['A'] to be 1
+    // expect P['A']['L']['prize'] to be 1
+    // expect P['A']['R']['B'] to be 1
+    // expect P['A']['B']['C'] to be 1
+    // expect P['B']['L']['A'] to be 1
+    // expect P['B']['B']['D'] to be 1
+    // expect P['A']['B']['C'] to be 1
+    console.log(P)
+
+  });
+});
+
+describe('deterministic rewardsAndTransitions',function(){
+  var rewardsAndTransitions = pavlov.rewardsAndTransitions(determ.observations,determ.rewards);
+  var R = rewardsAndTransitions[1];
+  var P = rewardsAndTransitions[0];
+
+  it('should produce the correct transitions',function(){
+    console.log(P)
+
   });
 });
